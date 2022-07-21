b954855a8eefb74f70d4c91c20064d9f1680ec05
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2022-07-21 22:26:24 +0530

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -8,11 +8,11 @@ class Development(Config):
     # the name to display in your alive message
     ALIVE_NAME = "Your value"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
-    DB_URI = "Your value"
+    DB_URI = "mongodb+srv://Devilharsha:Devilharsha@cluster0.exwkd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
-    STRING_SESSION = "Your value"
+    STRING_SESSION = "1BVtsOMABu6OXr8Cc55nbqq2UgfskIgw-NNupVHW65PQ98XRvg4Fi_a6gHQE8xGz3VRPcixdx9ambahFIp2m6wRAjd2lAvwNnwD8vmQjatNnHyIAuNZjf-jteGvJPww6iLVssU9N7rASyThAGeJB2mdTu3aas3GHemVd8juXVO9xOnyY6Kx3P_daYOiyg5GZ5SXUwgou0uO1Ad1Jt_M8vDPJzotqFvngiHt-0VOktoPlacf5s9FHmzH7vif5NiVpPU1Y5DT30RVedpXKOjgocp_r0vEQVQHHbXhi6VpVPQuOykjsXO9axPnbgEN_REzT6g4sbC7yN2MJm3NfPf-lJhzkN4mfQsMY="
     # create a new bot in @botfather and fill the following vales with bottoken
-    TG_BOT_TOKEN = "Your value"
+    TG_BOT_TOKEN = "5317920065:AAGWadLJ4IfcGqANBR3Yes4QUflJXaHvJ7o"
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
     PRIVATE_GROUP_BOT_API_ID = -100
     # command handler
